554d0eff6a215135e6ca63e1fc41d72dd36d978f
charmyin <976582265@qq.com>
2015-03-19 20:55:09 +0800

    wechat token

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    var query = req.query;
+    var signature = query.signature;
+    var echostr = query.echostr;
+    var timestamp = query['timestamp'];
+    var nonce = query.nonce;
+    var oriArray = new Array();
+    oriArray[0] = nonce;
+    oriArray[1] = timestamp;
+    oriArray[2] = "charmyintoken";//这里填写你的token
+    oriArray.sort();
+    var original = oriArray[0]+oriArray[1]+oriArray[2];
+    console.log("Original Str:"+original);
+    console.log("signature:"+signature);
+    var scyptoString = sha1(original);
+    if (signature == scyptoString) {
+        res.send(echostr);
+    }
+    else {
+        res.send("Bad Token!");
+    }
 });
 
 module.exports = router;
